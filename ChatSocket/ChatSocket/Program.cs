531a4c043031a5b8d4553f080a6d7686e9598f98
Robertt Stevenson <robertt947s@gmail.com>
2021-04-19 17:18:38 -0400

    Agregar socket

--- ChatSocket/ChatSocket/Program.cs
+++ ChatSocket/ChatSocket/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ChatSocket.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,39 @@ namespace ChatSocket
     {
         static void Main(string[] args)
         {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            ServerSocket serverSocket = new ServerSocket(puerto);
+
+            if (serverSocket.Iniciar())
+            {
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Esperando Cliente...");
+                    if(serverSocket.ObtenerCliente())
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine("Cliente Recibido");
+                        Console.WriteLine("S: Hola cliente");
+                        serverSocket.Escribir("Hola Cliente");
+                        Console.WriteLine("Escriba un mensaje");
+                        string mensaje = Console.ReadLine().Trim();
+                        Console.WriteLine("S:{0}",mensaje);
+                        serverSocket.Escribir(mensaje);
+
+                        string respuesta = serverSocket.Leer();
+                        Console.WriteLine("C:{0}", respuesta);
+
+                        serverSocket.CerrarConexion();
+                    }
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No se puede levantar el servidor, puerto ocupado");
+                Console.ReadKey();
+            }
         }
     }
 }
